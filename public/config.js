2153c81851456005f15b857b8b1db84c0147b64e
Hunter Padilla <Hpadilla2020@FAU.edu>
2023-07-17 13:38:36 -0400

    Database Read/Write Configuration

--- public/config.js
+++ public/config.js
@@ -1,3 +1,4 @@
+  //Firebase Config Information
 const firebaseConfig = {
     apiKey: "AIzaSyAW6xhxT_ycudLcNJL9OIt6Y5AAuzky-Po",
     authDomain: "taskbuddy-9c3f6.firebaseapp.com",
@@ -8,10 +9,12 @@ const firebaseConfig = {
     measurementId: "G-0KCSZMXRL0"
   };
 
+  //Initialize firebase, auth, and our db references.
   firebase.initializeApp(firebaseConfig);
   auth = firebase.auth();
   db = firebase.firestore();
 
+  //Signs User up
   function signUp(){
 
     email = document.getElementById("email").value;
@@ -24,6 +27,7 @@ const firebaseConfig = {
 
   }
 
+  //Logs User in
   function login(){
 
     email = document.getElementById("email").value;
@@ -34,6 +38,76 @@ const firebaseConfig = {
         alert("Logged In" + user.email)
 
     })
+  }
 
+  //Sets User data in firebase
+  function set(){
+    var Studentname = document.getElementById('name').value;
+    var age = document.getElementById('age').value;
+    var id = document.getElementById('id').value;
+    var semester = document.getElementById('semester').value;
+    var UserID = auth.currentUser.uid;
+
+    db.collection("User").doc(UserID).set({
+      Studentname: Studentname,
+      age: age,
+      id: id,
+      semester: firebase.firestore.FieldValue.arrayUnion(semester),
+    }).then(()=>{
+      console.log("Success!")
+    }).catch((error) => {
+      console.log("Error!")
+    });
+  }
+
+  //Updates User data in firebase
+  function update(){
+    var Studentname = document.getElementById('name').value;
+    var age = document.getElementById('age').value;
+    var id = document.getElementById('id').value;
+    var semester = document.getElementById('semester').value;
+    var UserID = auth.currentUser.uid;
+
+    db.collection("User").doc(UserID).update({
+      Studentname: Studentname,
+      age: age,
+      id: id,
+      semester: firebase.firestore.FieldValue.arrayUnion(semester),
+    }).then(()=>{
+      console.log("Success!")
+    }).catch((error) => {
+      console.log("Error!")
+    });
+  }
+
+  //Displays User data on the website
+  function show(){
+    var UserID = auth.currentUser.uid;
+
+    db.collection("User").doc(UserID).get().then((doc) => {
+      if (doc.exists) {
+
+        console.log("Document data: ", doc.data());
+
+        document.getElementById('nameDisplay').innerHTML = doc.data().Studentname;
+        document.getElementById('ageDisplay').innerHTML = doc.data().age;
+        document.getElementById('idDisplay').innerHTML = doc.data().id;
+        document.getElementById('semesterDisplay').innerHTML = doc.data().semester;
+
+      } else{
+        console.log("No Such Document");
+      }
+     
+
+    })
+  }
+
+  //Deletes User data from Firebase
+  function deleteData() {
+
+    var UserID = auth.currentUser.uid;
+    db.collection("User").doc(UserID).delete().then(() => {
+      console.log("Document Successfully deleted!");
+    }).catch((error) => {console.error("Error Removing document ", error)})
 
   }
